fb2ce8dee6a5a54c95b5d0b00e35da20e9334d43
Xavier Lin <syn.xavier.lin+github@gmail.com>
2016-11-04 03:26:40 -0400

    add data pos

--- Torch/NeuralTalk/comm.sh
+++ Torch/NeuralTalk/comm.sh
@@ -1,1 +1,2 @@
-th main.lua -dataset flickr8k -finetune 1 -finetuneAfter 50 -cnnType resnet -retrain pretrained/resnet-18.t7 -resetCNNlastlayer true
+th main.lua -data gen/Flicker8k_Dataset -dataset flickr8k -finetune 1 -finetuneAfter 50 -cnnType resnet -retrain pretrained/resnet-18.t7 -resetCNNlastlayer true
+

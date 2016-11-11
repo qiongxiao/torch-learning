99fdde97129ff8b5bf3ff4236223cb34d6382e44
Xavier Lin <syn.xavier.lin+github@gmail.com>
2016-11-10 21:18:00 -0500

    add merge figure

--- Torch/NeuralTalk/utils/utils.lua
+++ Torch/NeuralTalk/utils/utils.lua
@@ -1,14 +1,7 @@
---[[
---
---	some general operations
---
---]]
-
 local cjson = require 'cjson'
 
 local utils = {}
 
---  code from https://github.com/jcjohnson/torch-rnn
 function utils.readJson(path)
 	local f = io.open(path, 'r')
 	local s = f:read('*all')
@@ -48,6 +41,50 @@ function utils.spairs(t, order)
 			return keys[i], t[keys[i]]
 		end
 	end
+end
+
+function utils.findMaxScore(path)
+	local figure = utils.readJson(path)
+	local maxScore = {}
+	for k, v in pairs(figure) do
+		local name = v['layout']['title']
+		local data = v['data']
+		for s, t in pairs(data) do
+			name = name .. " - " .. t['name']
+			local smalldata = t['y']
+			local max, maxid = 0, 0
+			for u, w in pairs(smalldata) do
+				if w > max then
+					max = w
+					maxid = u
+				end
+			end
+			local maxt = {name = name, max = max, max_epoch = maxid}
+			table.insert(maxScore, maxt)
+		end
+	end
+	return maxScore
+end
+
+function utils.mergeFigures(paths)
+	local finalFigure = {}
+	for _, path in pairs(paths) do
+		local filename = string.sub(path, 1, string.find(path, '.json')-1)
+		local figure = utils.readJson(path)
+		for title, subfig in pairs(figure) do
+			if not finalFigure[title] then
+				finalFigure[title] = subfig
+			else
+				local lines = subfig['data']
+				for _, data in pairs(lines) do
+					local name = filename .. ':' .. data['name']
+					data['name'] = name
+					table.insert(finalFigure[title]['data'], data)
+				end
+			end
+		end
+	end
+	utils.writeJson('merge.json', finalFigure)
 end
 
 return utils
